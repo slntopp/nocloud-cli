632d5384d1c5b76292482cf1b6a5c8c4ad112942
Mik <info@slnt-opp.xyz>
2021-11-05 13:48:59 +0100

    LICENSE

--- cmd/root.go
+++ cmd/root.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2021 slnt_opp
+Copyright © 2021 Nikita Ivanovski
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
