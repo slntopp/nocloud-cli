42220897a121a7304f8f39de6d080e1d89817567
Mik <info@slnt-opp.xyz>
2021-11-05 15:48:43 +0100

    handle default config

--- cmd/root.go
+++ cmd/root.go
@@ -54,7 +54,7 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.nocloud.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.default.nocloud.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
@@ -71,16 +71,27 @@ func initConfig() {
 		home, err := os.UserHomeDir()
 		cobra.CheckErr(err)
 
-		// Search config in home directory with name ".nocloud-cli" (without extension).
+		// Search config in home directory with name ".default.nocloud" (without extension).
 		viper.AddConfigPath(home)
 		viper.SetConfigType("yaml")
-		viper.SetConfigName(".nocloud")
+		viper.SetConfigName(".default.nocloud")
+
+		cfgFile = home + "/.default.nocloud.yaml"
 	}
 
 	viper.AutomaticEnv() // read in environment variables that match
 
+	_, err := os.Stat(cfgFile)
+	if !os.IsExist(err) {
+		if _, err := os.Create(cfgFile); err != nil { // perm 0666
+			fmt.Fprintln(os.Stderr, "Can't create default config file")
+			panic(err)
+		}
+	}
+
 	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
+	err = viper.ReadInConfig();
+	if err == nil {
 		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
 	}
 }
