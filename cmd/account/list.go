073543489f086fa97a2a991ff6e22e106a63c9a9
Mik <info@slnt-opp.xyz>
2021-11-08 23:12:14 +0100

    mark optional parameters

--- cmd/account/list.go
+++ cmd/account/list.go
@@ -25,7 +25,7 @@ import (
 
 // ListCmd represents the list command
 var ListCmd = &cobra.Command{
-	Use:   "list [NAMESPACE]",
+	Use:   "list [[NAMESPACE]] [[flags]]",
 	Short: "List NoCloud Accounts",
 	Long: `Add namespace UUID after list command, to filter accounts by namespace`,
 	RunE: func(cmd *cobra.Command, args []string) error {
