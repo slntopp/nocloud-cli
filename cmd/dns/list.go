aa73ea744335e9227f166dfd4a4088998d085bf7
Mik <info@slnt-opp.xyz>
2022-02-17 12:46:07 +0100

    dns: more aliases

--- cmd/dns/list.go
+++ cmd/dns/list.go
@@ -26,6 +26,7 @@ import (
 // ListCmd represents the list command
 var ListCmd = &cobra.Command{
 	Use:   "list",
+	Aliases: []string{"l"},
 	Short: "List NoCloud DNS Zones",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		ctx, client := MakeDNSClientOrFail()
