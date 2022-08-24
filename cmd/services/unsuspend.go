57bb9349077619926da153bfd8417e05ccf3584a
Jahor Laryn <mrojahor@gmail.com>
2022-08-24 19:43:00 +0300

    Handle suspend and unsuspend

---
+++ cmd/services/unsuspend.go
@@ -0,0 +1,37 @@
+/*
+Copyright © 2021-2022 Nikita Ivanovski info@slnt-opp.xyz
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package services
+
+import (
+	pb "github.com/slntopp/nocloud/pkg/services/proto"
+	"github.com/spf13/cobra"
+)
+
+// Unsuspend represents the unsuspend command
+var UnsuspendCmd = &cobra.Command{
+	Use:     "unsuspend [service_id] [flags]",
+	Aliases: []string{"unsus"},
+	Short:   "NoCloud Service Unsuspend",
+	Args:    cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) (err error) {
+		ctx, client := MakeServicesServiceClientOrFail()
+
+		req := pb.UnsuspendRequest{Uuid: args[0]}
+
+		_, err = client.Unsuspend(ctx, &req)
+		return err
+	},
+}
